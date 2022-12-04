d6a307c84d4f75e3944aa563dad637f53fdb3d1b
Alexey Odinochenko <conik9999@mail.ru>
2022-12-04 22:16:26 +0300

    feat: init

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,14 +1,24 @@
-const increaseButtonElement = [...document.querySelectorAll(".increaseCountIcon")];
-const countElements = [...document.querySelectorAll('.counter-text')];
-const decreaseButtonElement = [...document.querySelectorAll(".decreaseCountIcon")];
 
-increaseButtonElement.forEach((b, index) => {
-    b.addEventListener('click', () => {
-        countElements[index].innerHTML = +(countElements[index].innerHTML) + 1
+function handlePostCounter() {
+    const increaseButtonElement = [...document.querySelectorAll(".increaseCountIcon")];
+    const countElements = [...document.querySelectorAll('.counter-text')];
+    const decreaseButtonElement = [...document.querySelectorAll(".decreaseCountIcon")];
+
+    increaseButtonElement.forEach((b, index) => {
+        b.addEventListener('click', () => {
+            countElements[index].innerHTML = +(countElements[index].innerHTML) + 1
+        })
     })
-})
-decreaseButtonElement.forEach((b, index) => {
-    b.addEventListener('click', () => {
-        countElements[index].innerHTML = +(countElements[index].innerHTML) - 1
+    decreaseButtonElement.forEach((b, index) => {
+        b.addEventListener('click', () => {
+            countElements[index].innerHTML = +(countElements[index].innerHTML) - 1
+        })
     })
+}
+
+document.addEventListener('DOMSubtreeModified', () => {
+
+    handlePostCounter()
 })
+
+handlePostCounter()
